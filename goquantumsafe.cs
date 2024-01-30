e3a5bf50f82f5b4d1ad7249f83374fb04c192141
Aline Hommerding Amorim <ahamorim@inf.ufrgs.br>
2024-01-30 19:16:32 -0300

    Update functions to use byte[]

--- goquantumsafe.cs
+++ goquantumsafe.cs
@@ -4,6 +4,7 @@ using System;
 using OpenQuantumSafe;
 using System.Security.Cryptography.X509Certificates;
 using System.Collections;
+using System.Text;
 
 namespace quantum;
 
@@ -25,37 +26,19 @@ public partial class goquantumsafe : EditorPlugin
 		return new KEM(kem);
 	}
 
-	public void GetKeys(KEM kem, out CryptoKey cryptoPublicKey, out CryptoKey cryptoPrivateKey)
+	public void GetKeys(KEM kem, out byte[] publicKey, out byte[] privateKey)
     {
-		byte[] publicKey;
-		byte[] privateKey;
         kem.keypair(out publicKey, out privateKey);
-
-        string stringPublicKey = System.Text.Encoding.UTF8.GetString(publicKey);
-        cryptoPublicKey = new CryptoKey();
-        cryptoPublicKey.LoadFromString(stringPublicKey);
-
-        string stringPrivateKey = System.Text.Encoding.UTF8.GetString(privateKey);
-        cryptoPrivateKey = new CryptoKey();
-        cryptoPrivateKey.LoadFromString(stringPrivateKey);
     }
 
-	public CryptoKey Encapsulate(KEM kem, out byte[] cipherText, out byte[] sharedSecret, byte[] publicKey)
+	public void Encapsulate(KEM kem, out byte[] cipherText, out byte[] sharedSecret, byte[] publicKey)
 	{
 		kem.encaps(out cipherText, out sharedSecret, publicKey);
-        string stringSharedSecret = System.Text.Encoding.UTF8.GetString(sharedSecret);
-        CryptoKey cryptoKey = new CryptoKey();
-        cryptoKey.LoadFromString(stringSharedSecret);
-        return cryptoKey;
     }
 
-    public CryptoKey Decapsulate(KEM kem, byte[] cipherText, out byte[] sharedSecret, byte[] secretKey)
+    public void Decapsulate(KEM kem, byte[] cipherText, out byte[] sharedSecret, byte[] privateKey)
     {
-        kem.decaps(out sharedSecret, cipherText, secretKey);
-        string stringSharedSecret = System.Text.Encoding.UTF8.GetString(sharedSecret);
-		CryptoKey cryptoKey = new CryptoKey();
-		cryptoKey.LoadFromString(stringSharedSecret);
-		return cryptoKey;
+        kem.decaps(out sharedSecret, cipherText, privateKey);
     }
 }
 #endif
